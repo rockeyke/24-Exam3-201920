f10a82c314a9657d2137e97e007a7ad8e44d123c
rockeyke <rockeyke@rose-hulman.edu>
2019-01-31 20:26:31 -0500

    Done

--- src/problem1.py
+++ src/problem1.py
@@ -3,9 +3,9 @@ Exam 3, problem 1.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Kirsten Rockey.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -139,6 +139,17 @@ def problem1(n):
     # and THEN try the full-credit version.
     # As always, CONTINUE to the next problem if you are STUCK on this one.
     # -------------------------------------------------------------------------
+    fib_seq = [0]
+    counter = 0
+    while True:
+        if fib_seq[counter] >= n:
+            return fib_seq[counter-1]
+        elif counter == 0:
+            fib_seq.append(1)
+            counter += 1
+        else:
+            fib_seq.append(fib_seq[counter-1]+fib_seq[counter])
+            counter += 1
 
 
 ###############################################################################
